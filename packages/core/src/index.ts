a747d3b63e5638272ace75a68e936841b8c04c5e
QuentinDutot <dutot.quentin@gmail.com>
2025-05-09 17:22:24 +0200

    move vite plugins to core package

--- packages/core/src/index.ts
+++
@@ -1,4 +0,0 @@
-// biome-ignore lint/performance/noBarrelFile: <explanation>
-export { defineConfig, type GraniteConfig } from './definitions/config-builder'
-export { createServer, type ServerConfig } from './definitions/server-factory'
-// export { useLocation, useParams, A } from '@solidjs/router'
---
+++ packages/core/src/plugins.ts
@@ -0,0 +1,105 @@
+import { readFile, writeFile } from 'node:fs/promises'
+import { resolve } from 'node:path'
+import { createRequestAdapter, sendResponse } from '@universal-middleware/express'
+import type { Plugin, RunnableDevEnvironment, UserConfig } from 'vite'
+import { type Options as SolidPluginOptions, default as solidPlugin } from 'vite-plugin-solid'
+
+export interface SolidOptions extends Pick<SolidPluginOptions, 'babel' | 'include' | 'exclude'> {}
+
+export const solid = (options?: Partial<SolidOptions>): Plugin =>
+  solidPlugin({
+    ssr: true,
+    babel: options?.babel,
+    include: options?.include,
+    exclude: options?.exclude,
+  })
+
+// biome-ignore lint/suspicious/noEmptyInterface: <explanation>
+export interface GraniteOptions {}
+
+export const granite = (_options?: Partial<GraniteOptions>): Plugin => ({
+  name: 'granite',
+  config(config: UserConfig) {
+    config.appType = 'custom'
+    config.environments = {
+      client: {
+        consumer: 'client',
+        resolve: { noExternal: true },
+        build: {
+          manifest: true,
+          outDir: 'build',
+          assetsDir: 'assets',
+          emptyOutDir: true,
+          rollupOptions: { input: 'src/entry-client.tsx' },
+        },
+      },
+      server: {
+        consumer: 'server',
+        resolve: { noExternal: true },
+        build: {
+          outDir: 'build',
+          assetsDir: 'server',
+          emptyOutDir: false,
+          rollupOptions: { input: 'src/entry-server.tsx' },
+        },
+      },
+    }
+    config.builder = {
+      async buildApp(vite) {
+        await vite.build(vite.environments.client)
+
+        const manifestPath = resolve('build/.vite/manifest.json')
+        const manifestContent = await readFile(manifestPath, 'utf-8')
+        const manifestEntries = JSON.parse(manifestContent)
+
+        const normalizedManifest = Object.fromEntries(
+          Object.entries(manifestEntries).map(([key, value]) => {
+            const normalize = (path: string) => {
+              const filename = path.split('/').pop() ?? path
+              return filename.startsWith('src/') ? filename : `src/${filename}`
+            }
+
+            const normalizedKey = normalize(key)
+            // @ts-expect-error
+            const normalizedValue = { ...value }
+            if (normalizedValue.src) {
+              normalizedValue.src = normalize(normalizedValue.src)
+            }
+
+            return [normalizedKey, normalizedValue]
+          }),
+        )
+
+        await writeFile(manifestPath, JSON.stringify(normalizedManifest, null, 2), 'utf-8')
+
+        await vite.build(vite.environments.server)
+      },
+    }
+  },
+  async configureServer(vite) {
+    const serverEnvironment = vite.environments.server as RunnableDevEnvironment
+
+    return () => {
+      vite.middlewares.use(async (nodeRequest, nodeResponse) => {
+        const entry = await serverEnvironment.runner.import('src/entry-server.tsx')
+
+        const request: Request = createRequestAdapter()(nodeRequest)
+        const response: Response = await entry.default.fetch(request)
+
+        sendResponse(response, nodeResponse)
+      })
+    }
+  },
+  async configurePreviewServer(vite) {
+    return () => {
+      vite.middlewares.use(async (nodeRequest, nodeResponse) => {
+        const entry = await import(resolve('build/entry-server.js'))
+
+        const request: Request = createRequestAdapter()(nodeRequest)
+        const response: Response = await entry.default.fetch(request)
+
+        sendResponse(response, nodeResponse)
+      })
+    }
+  },
+})
