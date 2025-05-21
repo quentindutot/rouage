1fe25e978392bf6508a4cac903af38d94c704f91
QuentinDutot <dutot.quentin@gmail.com>
2025-05-21 23:11:51 +0200

    reorganize dev and prod dependencies

--- packages/solid-rouage/tsdown.config.ts
+++ packages/solid-rouage/tsdown.config.ts
@@ -1,13 +1,11 @@
 import { type Options, defineConfig } from 'tsdown'
-import packageJson from './package.json'
 
 const baseOptions: Options = {
   format: 'esm',
   outDir: 'build',
   clean: false,
   dts: true,
-  external: [/^virtual:/, ...Object.keys(packageJson.devDependencies), ...Object.keys(packageJson.peerDependencies)],
-  noExternal: [...Object.keys(packageJson.dependencies)],
+  external: [/^virtual:/],
 }
 
 // biome-ignore lint/style/noDefaultExport: <explanation>
